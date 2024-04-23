b0d088ee803b58359f82d248cfeff3b03559a60d
Mohammad Hosein Khatami Panah <117949572+mhkhatamipanah@users.noreply.github.com>
2024-04-24 00:34:20 +0330

    Add files via upload

---
+++ components/Search.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+    router.push(`/${search}/`);
+  };
+  return (
+    <form
+      className="w-50 flex justify-center md:justify-between"
+      onSubmit={handleSubmit}
+    >
+      <input
+        className="bg-white p-2 w-80 text-xl rounded-xl"
+        type="text"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        placeholder="Search ..."
+      />
+      <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+        search
+      </button>
+    </form>
+  );
+};
+
+export default Search;
